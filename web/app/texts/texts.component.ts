5307ff11a9f04b16bc710e4e6703e9c5efd7a708
Daniel Bozik <ext.daniel.bozik@tieto.com>
2019-10-28 16:44:12 +0100

    texts filtering - init

--- web/app/texts/texts.component.ts
+++ web/app/texts/texts.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { Subject } from 'rxjs';
 import { startWith, switchMap, takeUntil } from 'rxjs/operators';
 import { Text } from '../../../app/Objects';
@@ -13,28 +14,31 @@ import { TextService } from '../services/text.service';
 })
 export class TextsComponent implements OnInit, OnDestroy {
     public texts: Text[] = [];
+    public active: 'regular' | 'archived' = 'regular';
+    public filterForm: FormGroup;
+
     private componentDestroyed$: Subject<boolean> = new Subject<boolean>();
 
     constructor(
         private readonly languageService: LanguageService,
         private readonly textService: TextService,
         private readonly changeDetection: ChangeDetectorRef,
+        private readonly formBuilder: FormBuilder,
     ) {
     }
 
     ngOnInit() {
+        this.filterForm = this.formBuilder.group({
+            title: '',
+            createdFrom: '',
+            createdTo: '',
+        });
+
         this.languageService.languageSelected$.pipe(
             startWith(true),
             takeUntil(this.componentDestroyed$),
-            switchMap(() => {
-                return this.textService.getTexts();
-            })
-        ).subscribe((texts: Text[]) => {
-            this.texts = texts.sort(
-                (first, second) =>
-                    (new Date(second.createdOn)).getTime() - (new Date(first.createdOn)).getTime()
-            );
-            this.changeDetection.detectChanges();
+        ).subscribe(() => {
+            this.getRegularTexts();
         });
     }
 
@@ -46,6 +50,27 @@ export class TextsComponent implements OnInit, OnDestroy {
 
     public textClick(textId: string): void {
         this.textService.openText(textId);
+    }
+
+
+    public getRegularTexts(): void {
+        this.active = 'regular';
+
+        this.textService.getTexts().subscribe((texts: Text[]) => {
+            this.texts = texts.sort(
+                (first, second) =>
+                    (new Date(second.createdOn)).getTime() - (new Date(first.createdOn)).getTime()
+            );
+            this.changeDetection.detectChanges();
+        });
+    }
+
+
+    public getArchivedTexts(): void {
+        this.active = 'archived';
+
+        this.texts = [];
+        this.changeDetection.detectChanges();
     }
 
 }
